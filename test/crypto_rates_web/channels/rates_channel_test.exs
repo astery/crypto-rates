cfcc652196c5091f0ad775fd2a553730c34b1293
Andrey Akulov <andreyshark@gmail.com>
2018-03-19 14:53:41 +0300

    Add test with broadcast date

--- test/crypto_rates_web/channels/rates_channel_test.exs
+++ test/crypto_rates_web/channels/rates_channel_test.exs
@@ -5,15 +5,46 @@ defmodule CryptoRatesWeb.RatesChannelTest do
     Rate,
     Rates
   }
-  alias CryptoRatesWeb.UserSocket 
+  alias CryptoRatesWeb.UserSocket
 
-  test "should receive all rates saves" do
+  setup do
     {:ok, socket} = connect(UserSocket, %{})
-    {:ok, _, _socket} = subscribe_and_join(socket, "rates", %{})
+    {:ok, _, socket} = subscribe_and_join(socket, "rates", %{})
+    %{socket: socket}
+  end
 
-    rates = [%Rate{from: "BTC", to: "USD", rate: 10.0, at: from_naive!(~N[2018-01-01 02:00:00.000000])}]
-    :ok = Rates.save_rates(rates)
+  test "should receive all rates saves before broadcast date was set", %{socket: socket} do
+    first_rates = [
+      %Rate{from: "BTC", to: "USD", rate: 10.0, at: from_naive!(~N[2018-01-01 00:00:00.000000])},
+      %Rate{from: "ETH", to: "USD", rate: 10.0, at: from_naive!(~N[2018-01-01 00:00:00.000000])},
+    ]
+    :ok = Rates.save_rates(first_rates)
+    assert_broadcast("crypto_rates", %{rates: ^first_rates})
 
-    assert_broadcast("crypto_rates", %{rates: ^rates})
+    second_rates = [
+      %Rate{from: "BTC", to: "USD", rate: 20.0, at: from_naive!(~N[2018-01-01 01:00:00.000000])},
+      %Rate{from: "ETH", to: "USD", rate: 20.0, at: from_naive!(~N[2018-01-01 01:00:00.000000])},
+    ]
+    :ok = Rates.save_rates(second_rates)
+    assert_broadcast("crypto_rates", %{rates: ^second_rates})
+
+    # should receive nearest rates
+    date = from_naive!(~N[2018-01-01 01:00:00.000000])
+    push socket, "set_broadcast_date", %{"date" => DateTime.to_string(date)}
+    assert_broadcast("broadcast_date_set", %{date: date})
+    assert_broadcast("crypto_rates", %{rates: ^first_rates})
+
+    # should stay mute
+    third_rates = [
+      %Rate{from: "BTC", to: "USD", rate: 30.0, at: from_naive!(~N[2018-01-01 02:00:00.000000])},
+      %Rate{from: "ETH", to: "USD", rate: 30.0, at: from_naive!(~N[2018-01-01 02:00:00.000000])},
+    ]
+    :ok = Rates.save_rates(second_rates)
+    refute_broadcast("crypto_rates", %{rates: ^third_rates})
+
+    # should continue receive rate saves
+    push socket, "set_broadcast_date", %{"date" => ""}
+    assert_broadcast("broadcast_date_set", %{date: ""})
+    assert_broadcast("crypto_rates", %{rates: ^third_rates})
   end
 end
